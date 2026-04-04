2d00ee4f220e2f45699ddb8e1bb1dbf5d3a03b7c
jaketakula <jake.zhang.work@gmail.com>
2026-04-04 11:19:37 +1100

    chore: init workspace skeleton

--- fuzz/fuzz_targets/params.rs
+++ fuzz/fuzz_targets/params.rs
@@ -1,9 +1,7 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use proto::{Side, transport_parameters::TransportParameters};
 
-fuzz_target!(|data: &[u8]| {
-    let mut data = data;
-    let _ = TransportParameters::read(Side::Client, &mut data);
+fuzz_target!(|_data: &[u8]| {
+    // Skeleton: retype from `main` branch.
 });
--- quinn-proto/src/range_set/mod.rs
+++ quinn-proto/src/range_set/mod.rs
@@ -2,6 +2,3 @@ mod array_range_set;
 mod btree_range_set;
 #[cfg(test)]
 mod tests;
-
-pub(crate) use array_range_set::ArrayRangeSet;
-pub(crate) use btree_range_set::RangeSet;
